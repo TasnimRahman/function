abb0679fff2d4c58bf5a1fa0541fb6a8d6d4cb85
TasnimRahman <www.trjarin@gmail.com>
2021-03-25 08:25:07 +0600

    new functions

--- function.js
+++ function.js
@@ -1,3 +1,29 @@
+//unit conversion
+function converter(meter) {
+    var meter;
+    var Kilometer = meter / 1000;
+    return Kilometer;
+  }
+
+  var output = converter(200404);
+  document.write("Kilometer : " + output + "<br>"+"<br>");
+
+//Product price calculation
+function budgetProducts(mobile,laptop,camera) {
+    var mobile = mobile * 50;
+    var laptop = laptop * 100;
+    var camera = camera *20;
+
+    var total = mobile+laptop+camera;
+    return total;
+    }
+
+var result = budgetProducts(3,3,3);
+document.write("Total price : " + result + "<br>"+"<br>");
+
+
+
+
 //calculating rent
 function hotelRent(day) {
 
@@ -16,16 +42,24 @@ function hotelRent(day) {
     }
 
 var totalRent = hotelRent(16);
-document.write("Total rent = " + totalRent + "<br>");
-
+document.write("Total rent = " + totalRent + "<br>"+"<br>");
 
 
 
 //finding best friend
-var array = ["Tasnim Rahman", "Monisha Akter", "Mashruba Jabin", "Afsana Nahid", "Farha Islam", "Nibedita Shaha Dipa"];
+function bestFriend(array){
+    var count = toString(0);
+    for(var i=0; i<array.length; i++){
+        if(count.length<array[i].length){
+            count = array[i];
+        }
+    }
+    return count;
+}
+    
+    var friend = bestFriend(["Tasnim Rahman", "Monisha Akter", "Mashruba Jabin", "Afsana Nahid", "Farha Islam", "Nibedita Shaha Dipa"]);
+    document.write("Your best friend is " + friend);
 
-array.sort((a,b) => b.length - a.length);
 
-document.write("Your best friend is " + array[0]);
 
 
